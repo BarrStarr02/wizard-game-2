ba1c718e23a7eeef0a56fd985d86ae3528bda22a
Anthony Barrera <barr.star77@gmail.com>
2024-09-17 22:27:09 -0500

    attack animations

--- player.gd
+++ player.gd
@@ -4,24 +4,33 @@ extends CharacterBody2D
 
 @export var speed : float = 150.0
 @export var movement_threshold : float = 0.1  # Threshold for determining idle vs moving
-
+@export var attack_duration : float = 0.35
 # Node references
 @onready var animation_tree : AnimationTree = $AnimationTree
 @onready var animation_state : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+@onready var attack_timer : Timer = $AttackTimer
 
 # Variables to handle movement and animation
 var direction : Vector2 = Vector2.ZERO
 var last_direction : Vector2 = Vector2(0, 1)  # Default to down direction for idle state
+var is_attacking = false
+
 
 func _ready():
 	animation_tree.active = true
 
 func _process(_delta):
+	if is_attacking:
+		return
 	update_animation_parameters()
 
 func _physics_process(_delta):
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		await trigger_attack()
 	# Get input from arrow keys or WASD and round the direction to avoid precision issues
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").round().normalized()
+	
+
 
 
 	# If direction is not zero, update velocity
@@ -33,12 +42,27 @@ func _physics_process(_delta):
 
 	# Move the player
 	move_and_slide()
+	
+	
+func trigger_attack() -> void:
+	is_attacking = true
+	$AnimationTree.set("parameters/attack/blend_position", get_direction_blend_position(last_direction))
+	animation_state.travel("attack")
+	
+	if attack_timer:
+		attack_timer.start()
+		await attack_timer.timeout
+	
+	
+
+	is_attacking = false
 
 
 # Update the animation based on player movement
 func update_animation_parameters():
-	#round the direction for animation blending
-	var rounded_direction = direction.round()
+	if is_attacking:
+		$AnimationTree.set("parameters/attack/blend_position", get_direction_blend_position(last_direction))
+		animation_state.travel("attack")
 	if velocity.length() < movement_threshold:
 		# Player is idle, use the last known direction
 		set_idle_animation()
@@ -62,6 +86,7 @@ func set_walk_animation():
 	animation_state.travel("walk")
 
 # Function to determine the blend position based on input direction
+@warning_ignore("shadowed_variable")
 func get_direction_blend_position(direction: Vector2) -> Vector2:
 	# Returns a blend position for the 4 main directions (up, down, left, right)
 	if direction.x > 0:
